abc65cfd059cd497863458afb5661969de3f5f69
Duc Tan Hoang <89440534+tanhoang14@users.noreply.github.com>
2023-08-28 22:39:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const container= document.querySelector("#container");
 const baseURL="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-for(i=1;i<=898;i++){
+for(i=1;i<=1020;i++){
     const pokemon=document.createElement('div');
     const label=document.createElement('span');
     label.innerText=`#${i}`;
